67bdf76d73b672b3da33772167f7c21dfa6f11df
Vaishnavi Daware <vdaware166@gmail.com>
2021-10-20 21:07:43 +0530

    resume

---
+++ data/profile-dummy.js
@@ -0,0 +1,98 @@
+
+const profileData = {
+    title: 'Profile',
+    name: 'Vaishnavi Daware',
+    sub_title: 'TY CSE',
+    about: {
+      intro: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+      contact: {
+        email: 'vdaware166@gmail.com',
+        phone: '9307823589',
+        address: 'Nashik, Maharashtra.'
+      }
+    },
+    CareerObjective: [
+      {
+          desc: 'To secure a challenging position in a reputable organisation to expand my learning ,knowlege and skiils. To get self satisfaction and make my parents proud.'
+      }
+    ],
+    education: [
+      {
+        Course: 'SSC' ,
+        duration: '2017',
+        score: '93.40%'
+      },
+      {
+        Course: 'HSC',
+        duration: '2019',
+        score: '82.60%'
+       },
+      {
+        Course: 'CET',
+        duration: '2019',
+        score: '99.32 Percentile'
+      },
+      {
+        Course: 'Engineering',
+        duration: '2019-2023',
+        std: '1st year',
+        score: '8 (CGPA)',
+        std: '2nd year',
+        score: '7.9(CGPA)'
+      }
+
+    ],
+    skills: [
+      {
+        category: 'Technical-skill',
+        topics: ['c', 'c++', 'python', 'Data structure', 'Web Dev.']
+      },
+      {
+        category: 'Non-technical skill',
+        topics: [
+          'Drawing',
+          'clay-Art',
+          'Editing'
+        ]
+      }
+    ],
+    projects: [
+      {
+        title: 'WCE club management system',
+        Problemstatement: 'To cluster in all the club related management at one place through responsive web coupled with AI chatbots to serve the cause.',
+        desc: `Our college has many clubs functioning in multiple domains.Every club has their own working tree. Zooming out from the clubs, comes the administration of all the clubs. It involves scheduling and monitoring of events, finance and resource management which is hectic to handle single handedly. To enable smooth functioning and efficient management here is our web application that serves multiple purposes all at one place!
+        .`
+      },
+      {
+        title: 'Lorem Ipsum',
+        duration: 'Apr - Jul 1917',
+        link: 'https://github.com/',
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`
+      }
+    ],
+    activities: [
+      {
+        catagory: 'Extra-curricular',
+        details: [
+            'I have passed "Elementary" and "Intermediate exam with a great grade.',
+            'I like to play Badminton, Reading and watching Cricket.'
+        ]
+      },
+      {
+        organization: 'co-curricular',
+        details: [
+            'Programing workshop.',
+            'Organised club event(pace-Prudence).'
+        ]
+      }
+    ],
+    certifications: [
+      {
+        desc: `Certificate for Vaishnavi Vitthal Daware  for _MaTPO_WCE_Tiger_Webinar_ fe..._`,
+        link: 'https://mail.google.com/mail/u/0?ui=2&ik=9d21865ef6&attid=0.1&permmsgid=msg-f:1667477185924544520&th=172411aa7a19b808&view=att&disp=safe'
+      }
+    ],
+    events: []
+  };
